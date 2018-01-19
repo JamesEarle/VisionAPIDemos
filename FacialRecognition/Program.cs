01bc83e9b9f967a64fe1ec0e28365d2047048054
James Earle <jaearle@microsoft.com>
2018-01-19 12:51:34 -0800

    init commit, dotnet core app for facial recognition

---
+++ FacialRecognition/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FacialRecognition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
