7959205646ea4d305541dfcb36e1f98e0b2377a2
James Earle <jaearle@microsoft.com>
2018-01-19 19:03:01 -0800

    successfully taking photo with redirect

--- FacialRecognition/routes/index.js
+++ FacialRecognition/routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
+var NodeWebcam = require('node-webcam');
+
+const opts = {
+  width: 1280,
+  height: 720,
+  quality: 100,
+  delay: 0,
+  saveShots: true,
+  output: "jpeg",
+  device: false,
+  callbackReturn: "location",
+  verbose: false
+}    
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+exports.index = router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-module.exports = router;
+exports.snap = router.post('/snap', function(req, res, next) {
+  var webcam = NodeWebcam.create(opts);
+  webcam.capture('test-image', (err, data) => {
+    if(err) throw err;
+    console.log(data);
+  });
+  res.render('index', {title: "snapped"});
+});
+
+// module.exports = router;
