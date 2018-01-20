005e4f4f5cccbe1cc0695398b51344f0e49d3b60
James Earle <jaearle@microsoft.com>
2018-01-19 19:12:52 -0800

    trouble with navigator bug, might need reroute to a page with no camera access to avoid resource locks

--- FacialRecognition/routes/index.js
+++ FacialRecognition/routes/index.js
@@ -13,20 +13,22 @@ const opts = {
   device: false,
   callbackReturn: "location",
   verbose: false
-}    
+}
 
 /* GET home page. */
-exports.index = router.get('/', function(req, res, next) {
+exports.index = router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-exports.snap = router.post('/snap', function(req, res, next) {
+exports.snap = router.post('/snap', function (req, res, next) {
   var webcam = NodeWebcam.create(opts);
   webcam.capture('test-image', (err, data) => {
-    if(err) throw err;
+    if (err) throw err;
     console.log(data);
   });
-  res.render('index', {title: "snapped"});
+  res.render('index', { 
+    title: "snapped"
+  });
 });
 
 // module.exports = router;
